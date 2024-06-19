7a2171e64257c88529962d3f6cca5627628d5039
Kaan Sahin <kaan.sahin@active-group.de>
2024-06-19 16:30:47 +0200

    Add List intro file

---
+++ racket/list_intro.rkt
@@ -0,0 +1,15 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname list_intro) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+;;; Listen
+
+;; Eine Liste ist eins der Folgenden
+;; - eine leere Liste
+;; - eine nicht-leere Liste (Cons-List)
+
+;; Eine leere Liste besteht aus... nix?
+
+
+;; Eine nicht-leere Liste besteht aus
+;; - einer Zahl
+;; - einer weiteren Liste (der restlichen Zahlen)
