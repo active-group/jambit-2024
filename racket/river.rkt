0d1a575e71e07365b7645816a795d5348a058eaa
Kaan Sahin <kaan.sahin@active-group.de>
2024-06-19 14:36:51 +0200

    Add river.rkt

---
+++ racket/river.rkt
@@ -0,0 +1,62 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname river) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+;;; Daten mit Selbstbezug
+
+;; Ein Fluss ist eins der Folgenden:
+;; - Bach mit Ursprungsort
+;; - Zusammentreffen von zwei Flüssen
+
+(define fluss (signature (mixed bach zusammentreffen)))
+
+;; Ein Bach hat folgende Eigenschaften
+;; - Ursprungsort
+
+(define-record bach
+  make-bach
+  bach?
+  (bach-ursprung string))
+
+;; Ein Zusammentreffen hat folgende Eigenschaften
+;; - Ort des Zusammentreffens
+;; - Hauptfluss
+;; - Nebenfluss
+
+(define-record zusammentreffen
+  make-zusammentreffen
+  zusammentreffen?
+  (zusammentreffen-ort string)
+  (zusammentreffen-haupt fluss)
+  (zusammentreffen-neben fluss))
+
+(define eschach (make-bach "Heimliswald"))
+(define prim (make-bach "Dreifaltigkeitsberg"))
+(define schlichem (make-bach "Tieringen"))
+(define neckar1 (make-zusammentreffen "Rottweil" eschach prim))
+(define neckar2 (make-zusammentreffen "Epfendorf" neckar1 schlichem))
+
+;; Fließt Wasser von Ort in den Fluss?
+
+(: fließt? (string fluss -> boolean))
+(check-expect (fließt? "Dreifaltigkeitsberg" prim) #t)
+(check-expect (fließt? "Dreifaltigkeitsberg" neckar1) #t)
+(check-expect (fließt? "Dreifaltigkeitsberg" neckar2) #t)
+(check-expect (fließt? "Tieringen" eschach) #f)
+(check-expect (fließt? "Heimliswald" eschach) #t)
+(check-expect (fließt? "Heimliswald" prim) #f)
+(check-expect (fließt? "Heimliswald" neckar1) #t)
+
+(define fließt?
+  (lambda (ort fluss)
+    (cond
+      ((bach? fluss) (string=? ort (bach-ursprung fluss)))   
+      ((zusammentreffen? fluss)
+       (or
+        (string=? ort (zusammentreffen-ort fluss))
+        (fließt? ort (zusammentreffen-neben fluss))
+        (fließt? ort (zusammentreffen-haupt fluss)))))))
+
+
+
+
+
